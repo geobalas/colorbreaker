2ee01f598fc0037372f24a8f16f3d6960d516d3d
George Balasis <geobal87@yahoo.gr>
2016-02-01 02:01:02 +0200

    Changed package.json
    Changed the server port
    
    Added temp files to gitignore
    
    Added postinstall scripts
    
    Added bower to package.json
    
    Added bower to package.json
    
    Changed the name value
    
    Fixed the checkmark for mobile
    
    Fixed the checkmark for mobile
    
    Changed the checkmark to make it work on iOS, changed the viewport restrictions
    
    Changed the modal width on mobile
    
    Made the pins that dont belong to the last row not clickable

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/', function (req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, function () {
-  console.log('Server listening on port 3000...');
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Up and running...');
 });
