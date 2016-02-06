82072a0987a67892aac45c2bf4930d793f637f1f
George Balasis <geobal87@yahoo.gr>
2016-02-06 21:48:14 +0200

    Added the hidden combination in a closure

--- public/js/controllers/gameController.js
+++ public/js/controllers/gameController.js
@@ -2,18 +2,51 @@
     angular.module('colorbreaker')
     .controller('gameController', function($scope) {
         var emptyRow = [null, null, null, null].map(function() {return {color: '', active: false};}),
-            emptyFeedback = ['', '', '', '']
-            hiddenCombination = null;
+            emptyFeedback = ['', '', '', ''];
         $scope.colors = ['red', 'orange', 'yellow', 'green', 'teal', 'blue', 'purple', 'brown'];
         $scope.activePin = null;
         $scope.rows = [];
         $scope.victory = false;
 
-        function generateHiddenCombination() {
+        function generateFeedbackFunction() {
             var colors = angular.copy($scope.colors);
-            hiddenCombination = [null, null, null, null].map(function() {
+            var hiddenCombination = [null, null, null, null].map(function() {
                 return colors.splice(Math.floor(Math.random() * colors.length), 1)[0];
             });
+
+            return function() {
+                var feedbackPinIter = 0;
+                var lastRow = $scope.rows[$scope.rows.length-1];
+                hiddenCombinationClone = angular.copy(hiddenCombination);
+                // Checking for black pegs
+                lastRow.pins.forEach(function(pin, pinIndex) {
+                    if(hiddenCombinationClone[pinIndex] === pin.color) {
+                        lastRow.feedback[feedbackPinIter] = 'black';
+                        hiddenCombinationClone[pinIndex] = 'checked';
+                        feedbackPinIter++;
+                    }
+                });
+                // Checking for white pegs
+                lastRow.pins.forEach(function(pin, pinIndex) {
+                    var combinationPinIndex = hiddenCombinationClone.indexOf(pin.color);
+                    if(combinationPinIndex >= 0) {
+                        lastRow.feedback[feedbackPinIter] = 'white';
+                        hiddenCombinationClone[combinationPinIndex] = 'checked';
+                        feedbackPinIter++;
+                    }
+                });
+                lastRow.feedback.sort(function(a, b) {
+                    return !b || a < b ? -1 : 1;
+                });
+                var win = lastRow.feedback.every(function(color) {
+                    return color === 'black';
+                });
+                if(win) {
+                    $scope.victory = true;
+                } else {
+                    createNewRow();
+                }
+            }
         }
 
         function createNewRow() {
@@ -24,7 +57,7 @@
             $scope.victory = false;
             $scope.rows = [];
             createNewRow();
-            generateHiddenCombination();
+            $scope.giveFeedback = generateFeedbackFunction();
         };
 
         $scope.pinClicked = function(pin) {
@@ -64,37 +97,7 @@
             });
         };
 
-        $scope.giveFeedback = function() {
-            var feedbackPinIter = 0;
-            var lastRow = $scope.rows[$scope.rows.length-1];
-            hiddenCombinationClone = angular.copy(hiddenCombination);
-            lastRow.pins.forEach(function(pin, pinIndex) {
-                if(hiddenCombinationClone[pinIndex] === pin.color) {
-                    lastRow.feedback[feedbackPinIter] = 'black';
-                    hiddenCombinationClone[pinIndex] = 'checked';
-                    feedbackPinIter++;
-                }
-            });
-            lastRow.pins.forEach(function(pin, pinIndex) {
-                var combinationPinIndex = hiddenCombinationClone.indexOf(pin.color);
-                if(combinationPinIndex >= 0) {
-                    lastRow.feedback[feedbackPinIter] = 'white';
-                    hiddenCombinationClone[combinationPinIndex] = 'checked';
-                    feedbackPinIter++;
-                }
-            });
-            lastRow.feedback.sort(function(a, b) {
-                return !b || a < b ? -1 : 1;
-            });
-            var win = lastRow.feedback.every(function(color) {
-                return color === 'black';
-            });
-            if(win) {
-                $scope.victory = true;
-            } else {
-                createNewRow();
-            }
-        };
+        $scope.giveFeedback = generateFeedbackFunction();
 
         $scope.play();
     });
